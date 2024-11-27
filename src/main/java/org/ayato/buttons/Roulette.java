9c1f7e9dbc8defa5aca31563ba3d0cbcbf25673e
Ayato964 <chtk20063@g.nihon-u.ac.jp>
2024-11-27 15:07:07 +0900

    Ver.0.2.0
    ２つ目の抽選
    ルーレットを追加

---
+++ src/main/java/org/ayato/buttons/Roulette.java
@@ -0,0 +1,97 @@
+package org.ayato.buttons;
+
+import org.ayato.animation.PropertiesComponent;
+import org.ayato.system.Tick;
+import org.ayato.system.ToonMaster;
+import org.ayato.util.Display;
+import org.ayato.util.Setup;
+
+import java.awt.*;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Random;
+import java.util.function.Supplier;
+
+public class Roulette implements Setup, Display, Tick {
+    private final HashMap<Integer, String> rate = new HashMap<>();
+    private String[][] roulette = new String[3][3];
+    private final Random seed = new Random();
+    private int length;
+    private boolean isStartRoulette = false;
+    private int rCount = 0;
+    private int time = 0;
+    public Roulette(){
+        int c = 1;
+        int r = 0;
+        boolean isEnd = false;
+        while (!isEnd){
+            if(r % 2 == 1) {
+                rate.put(r, String.valueOf(c));
+                c ++;
+            }else{
+                rate.put(r, "*");
+            }
+            if(c != 10) {
+                r++;
+            }else {
+                isEnd = true;
+                length = r;
+            }
+        }
+        turning(-1);
+    }
+
+    private void turning(int al) {
+        for(int i = 0; i < 3; i ++){
+            int r = al == -1 ? seed.nextInt(length) : al;
+            roulette[1][i] = rate.get(r);
+            roulette[0][i] = rate.get(r - 1 < 0 ? length : r - 1);
+            roulette[2][i] = rate.get(r + 1 > length ? 0 : r + 1);
+        }
+    }
+
+    @Override
+    public void createUI(ToonMaster toonMaster) {
+        for(int i= 0; i < 3; i ++){
+            for(int c = 0; c < 3; c ++){
+                int finalI = i;
+                int finalC = c;
+                toonMaster.addAnimation((Supplier<String>) ()->roulette[finalI][finalC], PropertiesComponent.ofText(200 + c * 30, 40 + i * 30)
+                        .color(Color.WHITE).font("", 0, 4f));
+            }
+        }
+    }
+
+    @Override
+    public void display(Graphics graphics) {
+        graphics.setColor(Color.WHITE);
+        graphics.drawLine(650, 150, 650, 700);
+        graphics.drawLine(1250, 150, 1250, 700);
+        graphics.drawLine(650, 150, 1250, 150);
+        graphics.drawLine(650, 700, 1250, 700);
+    }
+    public void start(){
+        isStartRoulette = true;
+    }
+    @Override
+    public void tick() {
+        if(isStartRoulette){
+            turning(rCount);
+            rCount ++;
+            time ++;
+            if(rCount > length){
+                rCount = 0;
+            }
+            if(time >= 3000){
+                time = 0;
+                isStartRoulette = false;
+                turning(-1);
+            }
+        }
+    }
+
+    @Override
+    public long getSerialID() {
+        return 999;
+    }
+}
--- src/main/java/org/ayato/scene/MainGame.java
+++ src/main/java/org/ayato/scene/MainGame.java
@@ -2,9 +2,12 @@ package org.ayato.scene;
 
 import org.ayato.animation.PropertiesComponent;
 import org.ayato.buttons.ButtonLayer;
+import org.ayato.buttons.Roulette;
 import org.ayato.entities.Player;
+import org.ayato.system.Tick;
 import org.ayato.system.ToonMaster;
 import org.ayato.util.BaseScene;
+import org.ayato.util.Display;
 import org.ayato.util.Setup;
 
 import java.awt.*;
@@ -16,7 +19,7 @@ public class MainGame extends BaseScene {
     private Player player = new Player(1000);
     private final Random rand = new Random();
     private final ButtonLayer layer = new ButtonLayer(this::lottery);
-
+    private final Roulette roulette = new Roulette();
 
     @Override
     public void tick() {
@@ -33,18 +36,30 @@ public class MainGame extends BaseScene {
                 .font("", 0, 2f));
 
     }
-
     @Override
     public void setupUIClass(ArrayList<Setup> setups) {
         setups.add(layer);
+        setups.add(roulette);
     }
 
     private void lottery(int number){
         int lo = rand.nextInt(9) + 1;
-        if(lo == number)
-            player.addGold(5);
+        if(lo == number) {
+            player.addGold(4);
+            roulette.start();
+        }
 
         layer.chooseNumber = lo;
         player.removeGold(1);
+    }
+
+    @Override
+    public void displayClass(ArrayList<Display> display) {
+        display.add(roulette);
+    }
+
+    @Override
+    public void tickClass(ArrayList<Tick> ticks) {
+        ticks.add(roulette);
     }
 }
